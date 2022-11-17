7017d768f19ae42460219c2cdc7091075bd835e1
Roberto Foglia <roberto.foglia.computer.scientist@gmail.com>
2022-11-17 16:42:25 +0100

    Partial Functions

--- src/main/scala/lectures/part2afp/PartialFunctions.scala
+++ src/main/scala/lectures/part2afp/PartialFunctions.scala
@@ -1,6 +1,9 @@
 package lectures.part2afp
 
+import scala.io.Source.stdin
+import scala.util.Try
 
+// ## Partial Functions #############
 object PartialFunctions extends App {
 
   val aFunction = (x: Int) => x + 1 // Function1[Int, Int] === Int => Int
@@ -13,6 +16,7 @@ object PartialFunctions extends App {
 
   class FunctionNotApplicableException extends RuntimeException
 
+  // more niceer thea before, with pattern matching
   val aNicerFussyFunction = (x: Int) => x match {
     case 1 => 42
     case 2 => 56
@@ -26,17 +30,21 @@ object PartialFunctions extends App {
     case 5 => 999
   } // partial function value
 
+  // aNicerFussyFunction is equivalent to aPartialFunction
+
   println(aPartialFunction(2))
-//  println(aPartialFunction(57273))
+  //  println(aPartialFunction(57273))    // throws a MatchError and doesn't throws a FunctionNotApplicableException
 
   // PF utilities
-  println(aPartialFunction.isDefinedAt(67))
+  println(aPartialFunction.isDefinedAt(67)) // do we call without exceptions?
 
-  // lift
+  // lift - return the same function with a wrapper to an Option
   val lifted = aPartialFunction.lift // Int => Option[Int]
   println(lifted(2))
   println(lifted(98))
 
+
+  // Partial funtions chain
   val pfChain = aPartialFunction.orElse[Int, Int] {
     case 45 => 67
   }
@@ -51,7 +59,8 @@ object PartialFunctions extends App {
   }
 
   // HOFs accept partial functions as well
-  val aMappedList = List(1,2,3).map {
+  val aMappedList = List(1, 2, 3).map {
+    // it's a partial function
     case 1 => 42
     case 2 => 78
     case 3 => 1000
@@ -63,11 +72,53 @@ object PartialFunctions extends App {
    */
 
   /**
-    * Exercises
-    *
-    * 1 - construct a PF instance yourself (anonymous class)
-    * 2 - dumb chatbot as a PF
-  */
+   * Exercises
+   *
+   * 1 - construct a PF instance yourself (anonymous class)
+   * instancing anonymous class which derived from partial function
+   * 2 - dumb chatbot as a PF
+   */
+
+  // My implementation
+
+  // 1
+  //  val pFAnonymousClass = new PartialFunction {
+  //    val pf : PartialFunction[Int, Int] = {
+  //      case 1 => 42
+  //      case 2 => 56
+  //      case 5 => 999
+  //    }
+  //  }
+  //
+  //  pFAnonymousClass.pf.isDefinedAt(3)
+  // IT IS WRONG
+
+  // 2
+  // IT IS WRONG
+  //
+  //  class ChatBot with PartialFunction {
+  //
+  //  }
+  //
+  //  val myChatBot = new PartialFunction[Unit, Unit] {
+  //    override def apply(nothing: Unit): Unit = {
+  //    }
+  //
+  //    override def isDefinedAt(phrase: String): Boolean = {
+  //      Try(
+  //        apply(phrase)
+  //      ).isSuccess
+  //    }
+  //
+  //
+  //  }
+  //
+  //  scala.io.Source.stdin.getLines().foreach(
+  //    line => println(myChatBot.userSay(line))
+  //  )
+
+
+  // Solutions
 
   val aManualFussyFunction = new PartialFunction[Int, Int] {
     override def apply(x: Int): Int = x match {
@@ -84,8 +135,12 @@ object PartialFunctions extends App {
     case "hello" => "Hi, my name is HAL9000"
     case "goodbye" => "Once you start talking to me, there is no return, human!"
     case "call mom" => "Unable to find your phone without your credit card"
+    case _ => "I don't understand"
   }
 
-  scala.io.Source.stdin.getLines().map(chatbot).foreach(println)
+  def myPrintln(string: String): Unit = println(s"chat bot: $string")
+
+  stdin.getLines().map(chatbot).foreach(myPrintln)
+
 
 }
